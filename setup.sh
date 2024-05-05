81bd7bc0dcfe8e8ec5c3b92f8ec062d565682418
malcak <malcakx@gmail.com>
2024-05-05 17:12:45 -0500

    refactor(setup.sh): replace custom prompt for gum

--- setup.sh
+++ setup.sh
@@ -1,57 +1,60 @@
-export c_default="\033[0m"
-export c_blue="\033[1;34m"
-export c_magenta="\033[1;35m"
-export c_cyan="\033[1;36m"
-export c_green="\033[1;32m"
-export c_red="\033[1;31m"
-export c_yellow="\033[1;33m"
+#!/usr/bin/env bash
 
-prompt() {
-  case "${1}" in
-    "-s")
-      echo -e "  ${c_green}${2}${c_default}" ;;    # print success message
-    "-e")
-      echo -e "  ${c_red}${2}${c_default}" ;;      # print error message
-    "-w")
-      echo -e "  ${c_yellow}${2}${c_default}" ;;   # print warning message
-    "-i")
-      echo -e "  ${c_cyan}${2}${c_default}" ;;     # print info message
-    "-t")
-      echo -e "  ${c_magenta}${2}${c_default}" ;;     # print title message
-  esac
-}
+[[ -n "${ENABLE_DEBUG+x}" ]] && set -x
 
 remove_previous() {
+  # $1: file to remove
+  # $2: hard mode flag. if it's set to true, the existing config file will be
+  # remove. if it's set to false, the existing config will be backup
+
   if [[ -f "${1}" ]]; then
+    gum log -s -l info "Existing config file found." file "${1}"
     if [[ "${2}" == "true" ]]; then
-      prompt -i "Removing previous file"
+      [[ -n "${ENABLE_DEBUG+x}" ]] && gum log -l debug "Hard mode is enabled."
+      gum log -s -l info "Removing existing config file." file "${1}"
       rm -rf "${1}"
     else
-      prompt -i "Backing up previous file"
+      [[ -n "${ENABLE_DEBUG+x}" ]] && gum log -l debug "Hard mode is disabled."
+      gum log -l info "Backing up existing config file." file "${1}.backup"
       mv "${1}" "${1}.backup"
     fi
   fi
 }
 
 restore_previous() {
+  # $1: symbolic link to remove
+
   if [[ -h "${1}" ]]; then
-    prompt -i "Removing ${1} link"
+    gum log -s -l info "Removing existing symbolic link." file "${1}"
     rm -f "${1}"
 
     if [[ -f "${1}.backup" ]]; then
-      prompt -i "Restoring the backup file"
+      gum log -l info "Backup file found."
+      gum log -s -l info "Restoring backup file." file "${1}.backup"
       mv "${1}.backup" "${1}"
     else
-      prompt -i "Backup file not found"
+      gum log -s -l info "Backup file not found." file "${1}.backup"
     fi
   else
-    prompt -w "Link ${1} not found"
+    gum log -s -l warn "Symbolic link not found." file "${1}"
   fi
 }
 
-prompt -t "Installing Malcak Dotfiles!"
+# pre-checks
+if ! command -v gum &> /dev/null; then
+    echo "gum is not installed."
+    echo "gum is required, you can install gum from https://github.com/charmbracelet/gum"
+    exit 1
+fi
+
+# script start
+gum style \
+	--foreground 105 --border-foreground 057 --bold --border rounded \
+	--align center --width 40 --margin "0 0" --padding "1 2" \
+  'Installing Malcak Dotfiles!'
 
 readonly DOTFILES_REPO_PATH="$(dirname "$(readlink -m "${0}")")"
+gum log -s -l info "dotfiles repo directory found." path $DOTFILES_REPO_PATH
 
 # argument parsing
 while [[ $# -gt 0 ]]; do
@@ -74,67 +77,84 @@ while [[ $# -gt 0 ]]; do
       neofetch_config="true"; shift ;;
     -t|-tilix|--oh-my-zsh-theme)
       tilix_theme="true"; shift ;;
+    --debug)
+      ENABLE_DEBUG="true"; shift ;;
     *)
-      prompt -e "ERROR: Unrecognized installation option "${1}"."
+      gum log -s -l error "Unrecognized installation argument." argument "'${1}'"
       prompt_error="true"; shift ;;
   esac
 done
 
 if [[ "${prompt_error}" == "true" ]]; then
-  prompt -e "Argument parsing with errors, stopping execution"
+  gum log -l fatal "Argument parsing with errors, stopping execution."
   exit 1
+fi
+
+if [[ "${is_hard_mode}" == "true" ]]; then
+  gum log -l warn "Hard mode is enabled. Existing config files for the selected tools will be removed."
 fi
 
 # git config
 if [[ "${git_config}" == "true" ]]; then
   if [[ "${uninstall}" == "true" ]]; then
-    prompt -i "Uninstalling git cofig"
+    gum log -l info "Uninstalling Git cofig."
     restore_previous "${HOME}/.gitconfig"
-    prompt -s "Git config successfully uninstalled"
+    gum log -l info "Git config successfully uninstalled."
   else
-    prompt -i "Setting up git cofig"
+    gum log -l info "Setting up Git cofig."
     remove_previous "${HOME}/.gitconfig" "${is_hard_mode}"
+    gum log -s -l info "Creating symbolic link." file "${HOME}/.gitconfig"
     ln -s "${DOTFILES_REPO_PATH}/home/.gitconfig" "${HOME}/.gitconfig"
-    prompt -s "Git config set up correctly"
+    gum log -l info "Git config is set up."
   fi
 fi
 
-# zsh
+# zsh config
 if [[ "${zsh_config}" == "true" ]]; then
 
   ZSH_CONFIG_DIR="${ZDOTDIR:-${HOME}/.config/zsh}"
 
   if [[ "${uninstall}" == "true" ]]; then
-    prompt -i "Uninstalling ZSH cofig"
+    gum log -l info "Uninstalling ZSH cofig."
 
     restore_previous "${HOME}/.zshenv"
     if [[ -d "${ZSH_CONFIG_DIR}" ]]; then
+      gum log -s -l info "Config dir found." file "${ZSH_CONFIG_DIR}"
       restore_previous "${ZSH_CONFIG_DIR}/.zshrc"
       restore_previous "${ZSH_CONFIG_DIR}/.zaliases"
       restore_previous "${ZSH_CONFIG_DIR}/.zlogout"
     fi
 
-    prompt -s "ZSH config successfully uninstalled"
+    gum log  -l info "ZSH config successfully uninstalled."
   else
-    prompt -i "Setting up ZSH cofig"
+    gum log -l info "Setting up ZSH cofig."
     remove_previous "${HOME}/.zshenv" "${is_hard_mode}"
 
     if [[ -d "${ZSH_CONFIG_DIR}" ]]; then
+      gum log -s -l info "Config dir found." file "${ZSH_CONFIG_DIR}"
       remove_previous "${ZSH_CONFIG_DIR}/.zshrc" "${is_hard_mode}"
       remove_previous "${ZSH_CONFIG_DIR}/.zaliases" "${is_hard_mode}"
       remove_previous "${ZSH_CONFIG_DIR}/.zlogout" "${is_hard_mode}"
     else
+      gum log -l info "Config dir not found."
+      gum log -s -l info "Creating config dir." file "${ZSH_CONFIG_DIR}"
       mkdir -p "${ZSH_CONFIG_DIR}"
     fi
 
     if [[ -d "${HOME}/.cache/zsh" ]]; then
+      gum log -l info "Cache dir not found."
+      gum log -s -l info "Creating cache dir." file "${HOME}/.cache/zsh"
       mkdir -p "${HOME}/.cache/zsh"
     fi
 
+    gum log -s -l info "Creating symbolic link." file "${HOME}/.zshenv"
     ln -s "${DOTFILES_REPO_PATH}/shell/zsh/.zshenv" "${HOME}/.zshenv"
+    gum log -s -l info "Creating symbolic link." file "${ZSH_CONFIG_DIR}/.zshrc"
     ln -s "${DOTFILES_REPO_PATH}/shell/zsh/.zshrc" "${ZSH_CONFIG_DIR}/.zshrc"
+    gum log -s -l info "Creating symbolic link." file "${HOME}/.zaliases"
     ln -s "${DOTFILES_REPO_PATH}/shell/zsh/.zaliases" "${ZSH_CONFIG_DIR}/.zaliases"
+    gum log -s -l info "Creating symbolic link." file "${HOME}/.zlogout"
     ln -s "${DOTFILES_REPO_PATH}/shell/zsh/.zlogout" "${ZSH_CONFIG_DIR}/.zlogout"
-    prompt -s "ZSH config set up correctly"
+    gum log -l info "ZSH config is set up."
   fi
 fi
